4a9c6125c9caa7a3e7c5d25302d1c2767951ef06
magdalenamartinez <magdalena.martinezsanchez01@gmail.com>
2024-12-23 14:45:15 +0100

    day7 part2

--- day7/src/utilities.c
+++ day7/src/utilities.c
@@ -1,27 +1,33 @@
 #include "../include/day7.h"
+#include <math.h>
 
 int try_operation(int i, int j)
 {
-    if (i & (1 << j)) { 
-        return 1;
-    } else {
-        return 0;
-    }
+    int op = (i / (int)pow(3, j)) % 3;
+    return op;
 }
 
 long long is_the_number(list_numbers_t* list_numbers, int row)
 {
     int i = 0, multiply = 0;
     long long result = 0;
-    int n_operations = 1 << (list_numbers->num_row[row].length - 1);
-
+    int n_operations =(int)pow(3, list_numbers->num_row[row].length - 1);
     while (i < n_operations) {
         result = list_numbers->num_row[row].numbers[0];
         for (int j = 1; j < list_numbers->num_row[row].length; j++) {
-            if (try_operation(i, j - 1)) {
+            int value = try_operation(i, j - 1);
+            if (value == 2) {
                 result = result * list_numbers->num_row[row].numbers[j];
-            } else {
+            } else if (value == 1) {
                 result = result + list_numbers->num_row[row].numbers[j];
+            } else {
+                char str[200];
+                sprintf(str, "%ld", list_numbers->num_row[row].numbers[j]);
+                int n = 0;
+                while (str[n] != '\0') {
+                    result = result * 10 + (str[n] - '0');
+                    n++;
+                }
             }
         }
         if (result == list_numbers->num_row[row].result) {
@@ -29,7 +35,6 @@ long long is_the_number(list_numbers_t* list_numbers, int row)
         } 
         i++;
     }
-    
     return 0;
 }
 
